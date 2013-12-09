8dd6b0434cbf34f141b1c6d7570ce0b1b82e698d
ajosepha <ariel.isaacs@gmail.com>
2013-12-09 17:57:32 -0500

    delete test pages

--- Working_version/app.rb
+++ Working_version/app.rb
@@ -5,15 +5,6 @@ require './lib/compliment.rb'
 module StudentSite
   class App < Sinatra::Base
 
-    get '/hello-world' do
-      @random_numbers = [1, 2, 3, 4]
-      erb :hello
-    end
-
-    get '/artists' do
-        @artists = ["jacques dutronc", "jacques brel", "alex beaupain", "camille", "julien doré"]
-        erb :artists
-    end
 
     get '/compliment' do
       @compliment = Compliment.new.messages
